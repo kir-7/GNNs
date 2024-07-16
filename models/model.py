775c39cbe6e039b31af173f1fc5d96976205542d
kir_7 <kkireeti1@gmail.com>
2024-07-16 19:32:39 +0530

    a lot of issues with mgvae

--- models/model.py
+++ models/model.py
@@ -9,6 +9,7 @@ from torch_geometric.data import Data
 
 from models.layers import gLayer, gConv, GATConv
 
+from huggingface_hub import PyTorchModelHubMixin
 
 class GNN(nn.Module):
     def __init__(self, n_layers=4, emb_dim=50, edge_dim=4, in_dim=10, out_dim=1):
@@ -217,9 +218,9 @@ class GraphEncoder(MessagePassing):
 
         return out
 
-    
+
 class GraphDecoder(MessagePassing):
-    def __init__(self, filters : list, latent_dim, M, R, emb_dim, edge_dim, activation='leaky', norm='batch', add_self_loops=False, batch_size=32, dropout=0.3, negative_slope=0.2):
+    def __init__(self, filters : list, latent_dim, M, R, emb_dim, edge_dim, edge_classes=5, activation='leaky', norm='batch', add_self_loops=False, batch_size=32, dropout=0.3, negative_slope=0.2, device='cpu'):
 
         super().__init__()
 
@@ -230,6 +231,10 @@ class GraphDecoder(MessagePassing):
         self.edge_dim = edge_dim
         self.filters = filters
         self.batch_size = batch_size
+        self.edge_classes = edge_classes
+
+
+        self.device = device
 
         self.activation = {"swish": SiLU(), "relu": ReLU(), 'leaky':LeakyReLU(negative_slope)}[activation]
         self.normalization = {"layer": torch.nn.LayerNorm, "batch": torch.nn.BatchNorm1d}[norm]
@@ -266,7 +271,7 @@ class GraphDecoder(MessagePassing):
                 Linear(64, 16),
                 BatchNorm1d(16),
                 self.activation,
-                Linear(16, 4)
+                Linear(16, self.edge_classes)
         )
 
         self.lin_in_node = Linear(5, self.filters[0], bias=False, weight_initializer='glorot')
@@ -294,6 +299,7 @@ class GraphDecoder(MessagePassing):
         out = torch.nn.functional.softmax(out, dim=2)
         z_boa = torch.max(out, dim=2).indices
 
+
         x, edge_index, edge_attr, batch_map = self.construct_graph(z_boa, z, self.lin_in_edge)
 
 
@@ -318,15 +324,14 @@ class GraphDecoder(MessagePassing):
 
     def construct_graph(self, z_boa, z, project):
         batch_size, atom_types = z_boa.shape
-        device = z_boa.device
 
         # Calculate the total number of atoms
         total_atoms = z_boa.sum().int().item()
 
         # Create x: one-hot encoding of atom types
-        x = torch.zeros(total_atoms, atom_types, device=device)
+        x = torch.zeros(total_atoms, atom_types, device=self.device)
         start_idx = 0
-        batch_map = torch.zeros(total_atoms, dtype=torch.long, device=device)
+        batch_map = torch.zeros(total_atoms, dtype=torch.long, device=self.device)
 
         for i in range(batch_size):
             num_atoms_batch = z_boa[i].sum().int().item()
@@ -334,7 +339,7 @@ class GraphDecoder(MessagePassing):
 
             # One-hot encoding for atoms in this batch
             x[start_idx:end_idx] = torch.nn.functional.one_hot(
-                torch.arange(atom_types, device=device).repeat_interleave(z_boa[i].int()),
+                torch.arange(atom_types, device=self.device).repeat_interleave(z_boa[i].int()),
                 num_classes=atom_types
             )
 
@@ -354,11 +359,11 @@ class GraphDecoder(MessagePassing):
             end_idx = start_idx + num_atoms_batch
 
             # Fully connect atoms within this batch
-            batch_edge_index = torch.combinations(torch.arange(start_idx, end_idx, device=device), r=2).t()
+            batch_edge_index = torch.combinations(torch.arange(start_idx, end_idx, device=self.device), r=2).t()
             edge_index.append(batch_edge_index)
 
             # Project z for this batch to edge_attr_dim
-            batch_z_proj = project(z[i].float().unsqueeze(0))  # [1, edge_attr_dim]
+            batch_z_proj = project(z[i].float().unsqueeze(0))  # outputs [1, edge_attr_dim]
 
             # Repeat the projected z_boa for each edge in this batch
             num_edges_batch = batch_edge_index.shape[1]
@@ -373,9 +378,8 @@ class GraphDecoder(MessagePassing):
 
         return x, edge_index, edge_attr, batch_map
 
-
-class MGVAE(GAE):
-    def __init__(self, encoder, decoder, MAX_LOGSTD=10, batch_size=32, device='cpu'):
+class MGVAE(GAE, PyTorchModelHubMixin):
+    def __init__(self, encoder, decoder, MAX_LOGSTD=10, batch_size=32, start_annealing_epoch=1, end_annealing_epoch=None, total_epochs=100, class_weights=None, device='cpu'):
         super().__init__(encoder, decoder)
         self.encoder = encoder.to(device)
         self.decoder = decoder.to(device)
@@ -385,6 +389,21 @@ class MGVAE(GAE):
         self.MAX_LOGSTD = MAX_LOGSTD
         self.batch_size = batch_size
         self.M, self.R = self.decoder.M, self.decoder.R
+        self.current_epoch = 0
+        self.start_annealing_epoch, self.end_annealing_epoch = start_annealing_epoch, end_annealing_epoch
+
+        self.device = device
+
+        self.total_epochs = total_epochs
+        
+        if class_weights is None:
+            self.class_weights = torch.ones(self.decoder.edge_classes, deice=self.device)
+        else:
+            self.class_weights = class_weights.to(self.device)
+
+        if self.end_annealing_epoch is None:
+            self.end_annealing_epoch = int(0.7 * self.total_epochs)
+
 
         self.mean_layer = Linear(self.latent_dim, self.latent_dim, bias=False, weight_initializer='glorot')
         self.logvar_layer = Linear(self.latent_dim, self.latent_dim, bias=False, weight_initializer='glorot')
@@ -397,6 +416,7 @@ class MGVAE(GAE):
         self.logvar_layer.reset_parameters()
 
     def reparametrize(self, mu, logstd):
+
         if self.training:
             return mu + torch.randn_like(logstd) * torch.exp(logstd)
         else:
@@ -414,51 +434,137 @@ class MGVAE(GAE):
     def decode(self, z):
         return self.decoder(z)
 
-    def forward(self, x, edge_index, edge_attr, batch):
+    def forward(self, data):
+
+        x, edge_index, edge_attr, batch, edge_batch = data.x, data.edge_index, data.edge_attr, data.batch, data.batch[data.edge_index[0]]
 
 
-        mean, logvar = self.encode(x, edge_index, edge_attr, batch)
+        mean, logvar = self.encode(x, edge_index, edge_attr, edge_batch)
 
 
         z = self.reparametrize(mean, logvar)
 
         z_boa, edge_index_out, edge_attr_out, edge_batch_out = self.decode(z)
+        
+        true_edge_attr, pred_edge_attr = self.pad_edge_attr(edge_attr, edge_attr_out)
+        true_node_prob = self.create_atom_count_vectors(x, self.M, self.R, batch)
 
-        return z_boa, edge_attr_out, mean, logvar
+        loss, edge_loss, node_loss, kl_loss = self.molecular_loss(true_edge_attr, pred_edge_attr, true_node_prob, z_boa, mean, logvar)
 
-    def reconstruct_molecules(self, z_boa, edge_index, edge_attr, edge_batch, batch_size):
+        return loss, edge_loss, node_loss, kl_loss
 
-        reconstructed_molecules = []
 
-        for i in range(batch_size):
-            # Get nodes for this molecule
-            molecule_atoms = z_boa[i]
-            num_atoms = molecule_atoms.sum().int().item()
+    def predict(self, z, batch):
 
-            # Create x (atom types)
-            x = torch.zeros((num_atoms, z_boa.size(1)), dtype=torch.float)
-            atom_idx = 0
-            for atom_type in range(z_boa.size(1)):
-                num_of_this_atom = molecule_atoms[atom_type].int().item()
-                x[atom_idx:atom_idx+num_of_this_atom, atom_type] = 1
-                atom_idx += num_of_this_atom
+        molecules = []
 
-            # Get edges for this molecule
-            mask = edge_batch == i
-            molecule_edge_index = edge_index[:, mask]
-            molecule_edge_attr = edge_attr[mask]
+        with torch.no_grad():
+            z_boa, edge_index, edge_attr, edge_batch = self.decoder(z)
+            z_boa = z_boa.view(-1, self.M, self.R)
 
-            # Create one-hot encoded edge_attr
-            edge_attr_onehot = torch.zeros((molecule_edge_attr.size(0), 4), dtype=torch.float)
-            edge_attr_onehot.scatter_(1, molecule_edge_attr.unsqueeze(1), 1)
+            for i in range(batch):
+                # Create x (atom types)
+                x = torch.zeros((torch.sum(torch.argmax(z_boa[i], dim=1)), self.M), dtype=torch.float)
+                atom_idx = 0
+                for atom_type in range(self.M):
+                    num_of_this_atom = torch.argmax(z_boa[i], dim=1)[atom_type].int().item()
+                    x[atom_idx:atom_idx+num_of_this_atom, atom_type] = 1
+                    atom_idx += num_of_this_atom
 
-            # Create Data object
-            data = Data(
-                x=x,
-                edge_index=molecule_edge_index,
-                edge_attr=edge_attr_onehot
-            )
+                mask = edge_batch == i
+                molecule_edge_index = edge_index[:, mask]
+                molecule_edge_attr = edge_attr[mask, :]
+                # Create one-hot encoded edge_attr
+                edge_attr_onehot = torch.zeros((molecule_edge_attr.size(0), 4), dtype=torch.float)
 
-            reconstructed_molecules.append(data)
+                for i in range(molecule_edge_attr.size(0)):
 
-        return reconstructed_molecules
+                    edge_attr_onehot[i, int(torch.max(molecule_edge_attr[i]).item())] = 1
+
+                # Create Data object
+                data = Data(
+                    x=x,
+                    edge_index=edge_index,
+                    edge_attr=edge_attr_onehot
+                )
+                molecules.append(data)
+
+        return molecules
+
+    def pad_edge_attr(self, true_edge_attr, pred_edge_attr):
+        
+        # Define the "none" edge
+        none_edge = torch.tensor([1, 0, 0, 0], device=self.device, dtype=true_edge_attr.dtype)
+
+        # Determine the number of edges
+        true_edge_count = true_edge_attr.size(0)
+        pred_edge_count = pred_edge_attr.size(0)
+
+        # Pad true_edge_attr if necessary
+        if true_edge_count < pred_edge_count:
+            padding = none_edge.repeat(pred_edge_count - true_edge_count, 1)
+            true_edge_attr = torch.cat([true_edge_attr, padding], dim=0)
+
+        # Pad pred_edge_attr if necessary
+        if pred_edge_count < true_edge_count:
+            padding = none_edge.repeat(true_edge_count - pred_edge_count, 1)
+            pred_edge_attr = torch.cat([pred_edge_attr, padding], dim=0)
+
+        true_edge_attr = true_edge_attr.to(self.device)
+        pred_edge_attr = pred_edge_attr.to(self.device)
+        return true_edge_attr, pred_edge_attr
+
+    def create_atom_count_vectors(self, x, M, R, batch_map):
+
+        # Get unique batch indices
+        unique_batches = torch.unique(batch_map)
+        batch_size = unique_batches.size(0)
+
+        atom_count_vectors = torch.zeros(batch_size, M, R, dtype=torch.float).to(self.device)
+
+        for i, batch_id in enumerate(unique_batches):
+            # Mask for current batch
+            mask = (batch_map == batch_id)
+
+            # Get node features for the current batch
+            batch_x = x[mask]
+
+            # Sum the atom types for the current graph
+            atom_counts = torch.sum(batch_x[:, 0:5].transpose(0, 1), dim=1)
+            onehot_vectors = torch.zeros(5, R)
+
+            # Create masks for each atom type and set the corresponding positions to 1
+            for k in range(5):
+                atom_count = int(atom_counts[k].item())  # Convert to integer
+                if atom_count < R:
+                    onehot_vectors[k, atom_count] = 1
+                else:
+                    onehot_vectors[k, R - 1] = 1
+
+            atom_count_vectors[i, :] = onehot_vectors
+
+        return atom_count_vectors.view(-1, R)
+
+
+    def molecular_loss(self, true_edge_attr, pred_edge_attr, true_node_prob, pred_node_prob, mu, logvar, epsilon=1e-8):
+
+        lambda_e, lambda_a = 1.0, 1.0
+
+        #  calculate the edge loss using weighted cross entropy
+        edge_loss = F.cross_entropy(pred_edge_attr, true_edge_attr, weight=self.class_weights)
+
+        #  calculate the node loss
+        node_loss = F.cross_entropy(pred_node_prob, true_node_prob)
+
+        #  calculate the kld loss
+        kld_loss = -0.5 * torch.mean(torch.sum(1 + logvar - mu**2 - (logvar.exp() + epsilon)**2, dim=1), dim=0)
+
+        if self.start_annealing_epoch > self.current_epoch:
+            beta = 0
+        
+        else:
+            #  uniformly increase the weight of kld loss over the annealing period ( default to 70% of training epochs after completely annealed weight = 1)
+            beta = (self.current_epoch - self.start_annealing_epoch)/(self.end_annealing_epoch - self.start_annealing_epoch)
+            beta = min(beta, 1.0)
+
+        return lambda_e*edge_loss + lambda_a*node_loss + beta*kld_loss, lambda_e*edge_loss, lambda_a*node_loss, beta*kld_loss
